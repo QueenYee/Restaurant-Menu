7dd8030eb59be3d05ab2fda202cf588e501b65b8
Stanyale Riley <freedomside@yahoo.com>
2022-09-05 20:38:06 -0500

    first commit

---
+++ src/restaurant/Restaurant.java
@@ -0,0 +1,25 @@
+package restaurant;
+        import java.util.Scanner;
+
+public class Restaurant {
+    //  static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        //   public static void main () {
+        MenuItem HotDog = new MenuItem("Hot Dog", 2.99, "Meat with Bread", "appetizer", true);
+        MenuItem pizza = new MenuItem("Pizza", 11.99, "Cheese with Sauce", "main", false);
+
+        Menu restaurantMenu = new Menu();
+        restaurantMenu.addMenuItem(HotDog);
+        restaurantMenu.addMenuItem(pizza);
+        // System.out.println("PIZza");
+        for (MenuItem item : restaurantMenu.getMenuItems()) {
+            System.out.println(item.getName());
+        }
+
+        restaurantMenu.setLastUpdated();
+        System.out.println(restaurantMenu.getLastUpdated());
+    }
+    //}
+}
